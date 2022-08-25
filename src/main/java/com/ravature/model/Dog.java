0add567064af5ae6a54731105511bab47ddf9b82
Adam Elliott <adamelliott1982@gmail.com>
2022-08-25 14:43:28 -0500

    initial commit test

---
+++ src/main/java/com/ravature/model/Dog.java
@@ -0,0 +1,9 @@
+package com.ravature.model;
+
+public class Dog {
+	public int numLegs = 0;
+	
+	public void eat() {
+		System.out.println("Chomp Chomp");
+	}
+}
---
+++ src/main/java/com/revature/main/App.java
@@ -0,0 +1,12 @@
+package com.revature.main;
+
+import com.ravature.model.Dog;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Dog dog1 = new Dog();
+		dog1.eat();
+	}
+}
